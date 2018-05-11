82e6180da07439b8370db03c445aa3ef9f0aef67
zhijianzhang <mrjzhang@tencent.com>
2018-05-11 16:59:34 +0800

    mobx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {Button} from 'antd';
+import {inject, observer} from 'mobx-react'
 import './App.css';
 
+// @inject('pageStore') @observer
 class App extends Component {
   render() {
+    const name = this.props.pageStore
     return (
       <div className="App">
+        {/* <h2>{name}</h2> */}
         <Button type="primary">Button</Button>
       </div>
     );
---
+++ src/pages/page2.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const Page2 = () => {
+  return <div>
+    Page2
+  </div>
+}
+
+export default Page2
